7c5eb372af3e4b561d9e301b4a8c020b39f431f0
LC1007 <tonyg7212@gmail.com>
2023-09-15 14:33:16 +0200

    added middleware files

---
+++ api/middleware/passport.js
@@ -0,0 +1,43 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+const User = require('../model/User')
+
+passport.use(new LocalStrategy({ usernameField: "emailAdd", passwordField: "userPass" },
+
+    async (emailAdd, userPass, done) => {
+      try {
+        const user = await User.findUserByEmail(emailAdd);
+
+        if (!user) {
+          return done(null, false, { msg: "Incorrect email address" });
+        }
+
+        const passwordMatch = await bcrypt.compare(userPass, user.userPass);
+
+        if (!passwordMatch) {
+          return done(null, false, { msg: "Incorrect Password" });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.userID);
+});
+
+passport.deserializeUser(async (userID, done) => {
+  try {
+    const user = await User.findUserByID(userID);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+module.exports = passport
