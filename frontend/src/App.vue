f1ef5a242fd566ccc6f84e86b5dd13b49d41b1d5
LC1007 <tonyg7212@gmail.com>
2023-09-15 15:58:49 +0200

    added files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <router-view/>
+    <Footer />
   </div>
 </template>
 
+<script>
+import Footer from '@/components/FooterComp.vue'
+export default{
+  components:{
+    Footer
+  }
+}
+</script>
 
+<style>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,95 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useCookies } from 'vue3-cookies';
 
+const {cookies} = useCookies()
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/about",
+    name: "about",
+    component: () => import("../views/AboutView.vue"),
+  },
+  {
+    path: "/admin",
+    name: "admin",
+    component: () => import("../views/AdminView.vue"),
+    beforeEnter: () => {
+      if (!cookies.get("loggedInUser")) {
+        router.push({ name: "login" });
+      }
+    },
+  },
+  {
+    path: "/products",
+    name: "products",
+    component: () => import("../views/ProductsView.vue"),
+    // beforeEnter: () => {
+    //   if (!cookies.get("loggedInUser")) {
+    //     router.push({ name: "login" });
+    //   }
+    // },
+  },
+  {
+    path: "/product/:bmxID",
+    name: "product",
+    component: () => import("../views/SingleView.vue"),
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: () => import("../views/SignupView.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/LoginView.vue"),
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: () => import("../views/CartView.vue"),
+    beforeEnter: () => {
+      if (!cookies.get("loggedInUser")) {
+        router.push({ name: "login" });
+      }
+    },
+  },
+  {
+    path: "/test",
+    name: "test",
+    component: () => import("../views/TestAdmin.vue"),
+  },
+  {
+    path: "/testprod",
+    name: "test",
+    component: () => import("../views/TestProducts.vue"),
+  },
+  {
+    path: "/product/create",
+    name: "create",
+    component: () => import("../components/CreateProdComp.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: () => import("../views/ProfileView.vue"),
+  },
+  {
+    path: "/product/edit/:bmxID",
+    name: "editprod",
+    component: () => import("../components/ProductEdit.vue"),
+  },
+  // {
+  //   path: "/user/edit/:userID",
+  //   name: "edituser",
+  //   component: () => import("../components/UserEdit.vue"),
+  // },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
