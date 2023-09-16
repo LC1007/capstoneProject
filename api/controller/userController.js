005f82a511f83cb82c27dbe49c759228df651c9d
LC1007 <131274987+LC1007@users.noreply.github.com>
2023-09-16 02:13:26 +0200

    Update userController.js

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -106,13 +106,13 @@ module.exports = {
     async updateUser(req, res){
         try {
             const { userID } = req.params
-            const user = req.body
+            const data = req.body
 
-            if(user.userPass){
-                user.userPass = hashSync(user.userPass, 10)
+            if(data.userPass){
+                data.userPass = hashSync(data.userPass, 10)
             }
 
-            const editedUser = await User.updateUser(user, userID)
+            const editedUser = await User.updateUser(data, userID)
 
             return res.status(200).json({
               status: 200,
